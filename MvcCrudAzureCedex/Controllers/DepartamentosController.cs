baa03213a494f5fb6beb0c406e4b197a33e12666
serraguti <pacoserranox@gmail.com>
2023-06-21 12:44:01 +0200

    commit

--- MvcCrudAzureCedex/Controllers/DepartamentosController.cs
+++ MvcCrudAzureCedex/Controllers/DepartamentosController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCrudAzureCedex.Models;
+using MvcCrudAzureCedex.Services;
 
 namespace MvcCrudAzureCedex.Controllers
 {
     public class DepartamentosController : Controller
     {
+        private ServiceApiDepartamentos service;
+
+        public DepartamentosController(ServiceApiDepartamentos service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> DepartamentosServidor()
+        {
+            List<Departamento> departamentos = await this.service.GetDepartamentosAsync();
+            return View(departamentos);
+        }
+
         public IActionResult DepartamentosCliente()
         {
             return View();
