734841aaf3618c6d68550168e9624ab2d883775a
serraguti <pacoserranox@gmail.com>
2023-06-21 13:10:44 +0200

    commit

--- MvcCrudAzureCedex/Controllers/DepartamentosController.cs
+++ MvcCrudAzureCedex/Controllers/DepartamentosController.cs
@@ -19,6 +19,42 @@ namespace MvcCrudAzureCedex.Controllers
             return View(departamentos);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Departamento departamento)
+        {
+            await this.service.CreateDepartamentoAsync
+                (departamento.Nombre, departamento.Localidad);
+            //AL INSERTAR, VAMOS A DEVOLVERLO A LA VISTA DONDE ESTA 
+            //EL DIBUJO DE TODOS LOS DEPARTAMENTOS
+            return RedirectToAction("DepartamentosServidor");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Departamento departamento =
+                await this.service.FindDepartamentoAsync(id);
+            return View(departamento);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Departamento departamento)
+        {
+            await this.service.UpdateDepartamentoAsync
+                (departamento.IdDepartamento, departamento.Nombre, departamento.Localidad);
+            return RedirectToAction("DepartamentosServidor");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.service.DeleteDepartamentoAsync(id);
+            return RedirectToAction("DepartamentosServidor");
+        }
+
         public IActionResult DepartamentosCliente()
         {
             return View();
--- MvcCrudAzureCedex/Services/ServiceApiDepartamentos.cs
+++ MvcCrudAzureCedex/Services/ServiceApiDepartamentos.cs
@@ -1,6 +1,7 @@
 ﻿using MvcCrudAzureCedex.Models;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace MvcCrudAzureCedex.Services
 {
@@ -49,6 +50,108 @@ namespace MvcCrudAzureCedex.Services
                 {
                     return null;
                 }
+            }
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //NECESITAMOS LA PETICION
+                string request = "/api/departamentos/" + id; 
+                //LA PETICION NECESITA UNA URL DE BASE
+                client.BaseAddress = new Uri(this.UrlApi);
+                //LIMPIAMOS LAS CABECERAS
+                client.DefaultRequestHeaders.Clear();
+                //TIPO DE DATOS A CONSUMIR
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                //REALIZAMOS LA PETICION
+                HttpResponseMessage response =
+                    await client.GetAsync(request);
+                //COMPROBAMOS SI LA PETICION ES CORRECTA
+                if (response.IsSuccessStatusCode)
+                {
+                    Departamento departamento =
+                        await response.Content.ReadAsAsync<Departamento>();
+                    return departamento;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        //POST
+        public async Task CreateDepartamentoAsync
+            (string nombre, string localidad)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/departamentos";
+                //DEPENDIENDO DE LO QUE ESTEMOS CONSUMIENDO EN CLOUD
+                //ES DECIR, DEPENDIENDO DE DONDE ESTE ALOJADA EL API
+                //NOS PUEDE DAR ERROR UTILIZAR BaseAddress
+                //Ejemplos: Api Management, VM
+                string url = this.UrlApi + request;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                //DEBEMOS CREAR EL OBJETO MODEL PARA CONVERTIRLO A JSON
+                Departamento departamento = new Departamento();
+                departamento.IdDepartamento = 0;
+                departamento.Nombre = nombre;
+                departamento.Localidad = localidad;
+                //CONVERTIMOS EL OBJETO A JSON
+                string json = JsonConvert.SerializeObject(departamento);
+                //PARA ENVIAR LOS DATOS SE UTILIZA LA CLASE StringContent
+                StringContent content = new
+                    StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response =
+                    await client.PostAsync(url, content);
+                //PODRIAMOS HACER NUESTRO IF PARA DEVOLVER LLAMADAS PERSONALIZADAS
+            }
+        }
+
+        private StringContent 
+            ConvertStringContent(int id, string nombre, string localidad)
+        {
+            //DEBEMOS CREAR EL OBJETO MODEL PARA CONVERTIRLO A JSON
+            Departamento departamento = new Departamento();
+            departamento.IdDepartamento = id;
+            departamento.Nombre = nombre;
+            departamento.Localidad = localidad;
+            //CONVERTIMOS EL OBJETO A JSON
+            string json = JsonConvert.SerializeObject(departamento);
+            //PARA ENVIAR LOS DATOS SE UTILIZA LA CLASE StringContent
+            StringContent content = new
+                StringContent(json, Encoding.UTF8, "application/json");
+            return content;
+        }
+
+        public async Task UpdateDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/departamentos";
+                string url = this.UrlApi + request;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                StringContent content = this.ConvertStringContent(id, nombre, localidad);
+                HttpResponseMessage response = await
+                    client.PutAsync(url, content);
+            }
+        }
+
+        public async Task DeleteDepartamentoAsync(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "/api/departamentos/" + id;
+                string url = this.UrlApi + request;
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.Header);
+                HttpResponseMessage response = await
+                    client.DeleteAsync(url);
             }
         }
     }
